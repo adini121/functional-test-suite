c03ab84d0e41082583b59101d0f09ab76bae17e1
Tim Barker <timb@moodle.com>
2012-07-26 13:06:29 +0800

    Tidied up a bit after adding scripts and JSON files

--- scripts/nodestartup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DISPLAY=:0
-/usr/bin/java -jar apps/selenium-server-standalone-2.24.1.jar -role node -hub http://192.168.200.27:4444/grid/register -nodeConfig /var/lib/jenkins/apps/webconfig.txt -Dwebdriver.chrome.driver=/var/lib/jenkins/apps/chromedriver
-# > /tmp/gridnode.log 2>&1
--- scripts/startgridhub.sh
+++ scripts/startgridhub.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DISPLAY="nightly01.test.local"
-sudo java -jar /var/lib/jenkins/selenium-server-standalone-2.24.1.jar -role hub 2>1& > /var/lib/jenkins/tmp/gridhub.log
+java -jar ../lib/selenium-2.25.0/selenium-server-standalone-2.25.0.jar -role hub 2>1& > ../tmp/gridhub.log
