fb4b5daeaecf6c3d9a44d9121331c104abfc0871
Tim Barker <timb@moodle.com>
2012-07-28 18:53:01 +0800

    Added IE Server driver for 32 and 64 bit

--- scripts/startgridhub.sh
+++ scripts/startgridhub.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-DISPLAY="nightly01.test.local"
-java -jar ../lib/selenium-2.25.0/selenium-server-standalone-2.25.0.jar -role hub 2>1& > ../tmp/gridhub.log
+#DISPLAY="nightly01.test.local"
+java -jar ../lib/selenium-2.25.0/selenium-server-standalone-2.25.0.jar -role hub 
+# 2>1& > ../tmp/gridhub.log
