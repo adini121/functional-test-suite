32b6c89cb602dd3665ee8f9094b990ca55d5787a
Tim Barker <timb@moodle.com>
2012-08-10 14:32:27 +0800

    Made modifications to the node script

--- scripts/firefoxnodestartup.sh
+++ scripts/firefoxnodestartup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DISPLAY=:99 && /etc/init.d/xvfb start
+export DISPLAY=:99
 /usr/bin/java -jar ../lib/selenium-2.25.0/selenium-server-standalone-2.25.0.jar -role node -hub http://nightly01.test.local:4444/grid/register -nodeConfig ../properties/JSON/FirefoxNode/webconfig.txt
 # > /tmp/gridnode.log 2>&1
