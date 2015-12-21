67adf2a7b5fc5a06068c54b5bc3b7e17fb13f0a1
AdiMac <adamsken1221@gmail.com>
2015-12-21 12:46:33 +0100

    more changes

--- src/com/moodle/test/TestRunSettings.java
+++ src/com/moodle/test/TestRunSettings.java
@@ -5,9 +5,10 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.sql.*;
+
+import java.io.*;
+import java.util.Calendar;
 import java.util.Properties;
 
 public class TestRunSettings {
@@ -74,9 +75,84 @@ public class TestRunSettings {
 			//sm.startChromeDriver(chromeDriverLocation);
 			//sm.startFirefoxDriver();
 			driver = sm.getRemoteDriver();
-			//driver = sm.getChromeDriver();
-			//driver = sm.getFirefoxDriver();
-			driver.get(moodleHomePage);
+			String sessionID = driver.getSessionId().toString();
+
+		// Saving browser sessionID to mysql database
+
+			String dburl = "jdbc:mysql://localhost:3306/moodle_sessionIDs";
+			String uname = "root";
+			String dropQuery = "DROP TABLE IF EXISTS test_session_ids";
+			String sqlQuery = "CREATE TABLE IF NOT EXISTS test_session_ids("
+					+ "id int unsigned auto_increment not null,"
+					+ "session_id VARCHAR(60) not null,"
+					+ "date_created VARCHAR(100) not null,"
+					+ "primary key(id)"
+					+ ")";
+			String tblQuery = "INSERT INTO test_session_ids(date_created, session_id)" + "VALUES" + "(?, ?)";
+
+		//Get time stamp
+			Calendar calendar = Calendar.getInstance();
+			java.sql.Timestamp timestamp = new java.sql.Timestamp(calendar.getTime().getTime());
+			java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+			Connection conn = null;
+			PreparedStatement ps = null;
+			try
+			{
+				//Register the JDBC Driver Class
+				Class.forName("com.mysql.jdbc.Driver");
+				//Create the JDBC connection
+
+				conn = DriverManager.getConnection(dburl, uname, "");
+				ps = conn.prepareStatement(sqlQuery);
+				ps.execute();
+				conn = DriverManager.getConnection(dburl,uname, "");
+				ps = conn.prepareStatement(tblQuery);
+				ps.setString(1, sessionID);
+				ps.setString(2, String.valueOf(timestamp));
+				ps.execute();
+
+			}
+			catch (ClassNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+			catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				try
+				{
+					if(conn != null)
+					{
+
+						ps.close();
+						//Close the JDBC Connection
+						conn.close();
+
+					}
+			}
+				catch (SQLException e)
+				{
+					e.printStackTrace();
+				}
+		}
+
+		// Writing to file and saving it to dropbox
+		try {
+				FileWriter fileWriter = new FileWriter("/home/adi/Dropbox/TestResults/Moodle/sessionIds.txt", true);
+				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+				bufferedWriter.write("testDBsessionID: " + sessionID);
+				bufferedWriter.newLine();
+				bufferedWriter.close();
+			} catch (IOException e) {
+			// execption handling
+		}
+		//driver = sm.getChromeDriver();
+		//driver = sm.getFirefoxDriver();
+		System.out.println(sessionID);
+		driver.get(moodleHomePage);
 	}
 	@AfterClass
 	public static void Quit() {
